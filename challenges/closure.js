0cb66d28ec4e729c81efd430a8d287fcdc21ea1d
Lambda_School_Loaner_271 <lambda_school_loaner_271@Lambda-School-Loaner-271s-MacBook-Pro.local>
2020-07-20 14:36:32 -0400

    finished counter

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,4 +23,11 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
- 
+
+let sum =  0
+ function summation(num){
+  for(var i = 0; i <= num; i++)
+  sum += i;
+  console.log(sum);
+}
+ summation(4);
